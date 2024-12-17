cf1f9fc9496964b4abee6fa6b11b11a16671b735
Anton Shustikov <anton.shustikov@plusgrade.com>
2024-12-17 13:52:08 -0500

    Add Page Not Found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+	return (
+		<div className='mt-12 mx-auto w-1/2 text-4xl text-center'>
+			<h1 className='font-bold'>404</h1>
+			<div>This page could not be found</div>
+		</div>
+	)
+}
